d938eb2e005e64849c3f7f654701e1217b0cfaed
Tarek Abdennadher <tarek.abdennadher@primatec.lan>
2025-06-27 15:45:35 +0100

    cleaning code

--- utils/data_loader.py
+++ utils/data_loader.py
@@ -1,25 +1,3 @@
-'''from PyPDF2 import PdfReader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
-from docx import Document'''
-'''
-def load_docx(file_path):
-    doc = Document(file_path)
-    return "\n".join([para.text for para in doc.paragraphs])
-def load_and_split_pdf(file_path):
-    text = ""  # Initialize an empty string to store all text from the PDF
-    
-    with open(file_path, "rb") as f: # Open the PDF file in binary read mode
-        reader = PdfReader(f)  # Create a PdfReader object to read the PDF
-        for page in reader.pages:  # Loop through each page in the PDF
-            text += page.extract_text()
-    
-    text_splitter = RecursiveCharacterTextSplitter(
-        chunk_size=1000,
-        chunk_overlap=200,
-        length_function=len
-    )
-    return text_splitter.split_text(text) # Split the full extracted text into smaller chunks and return the list of chunks'''
-
 from pypdf import PdfReader
 from docx import Document
 from langchain.text_splitter import RecursiveCharacterTextSplitter
